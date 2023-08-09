9aa0590592b6906185414ca58c248a5d049cd982
José Santillano <jose.santillano7479@gmail.com>
2023-08-09 15:02:37 -0600

    Configuración inicial Spring Boot Initializr
    - Spring Web
    - Spring Data JPA
    - Lombok
    - H2 Database

---
+++ src/test/java/com/codellege/hotel/HotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codellege.hotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
