116783ad4d01f112ca1e9d7d544910c08ee7621f
José Santillano <jose.santillano7479@gmail.com>
2023-08-09 15:10:24 -0600

    Capa Model creada con Huesped y Reserva

---
+++ src/main/java/model/Huesped.java
@@ -0,0 +1,28 @@
+package model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Data
+public class Huesped {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idHuesped;
+
+    private String nombre;
+
+    private String apellido;
+
+    private String fechaNacimiento;
+
+    private String nacionalidad;
+
+    private String telefono;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    private List<Reserva> reservas = new ArrayList<>();
+}
---
+++ src/main/java/model/Reserva.java
@@ -0,0 +1,24 @@
+package model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class Reserva {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idReserva;
+
+    private String fechaEntrada;
+
+    private String fechaSalida;
+
+    private double valor;
+
+    private String formaPago;
+}
